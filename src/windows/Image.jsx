5003cf56b65b9ea2bc159f9e3507ab7f947da9bc
Nishanth <nishathtamil72@gmail.com>
2026-01-07 13:48:25 +0530

    implemented folders

---
+++ src/windows/Image.jsx
@@ -0,0 +1,33 @@
+import { WindowControlls } from "#components";
+import WindowWrapper from "#hoc/WindowWrapper";
+import useWindowStore from "#store/windows";
+
+const ImageWindowContent = () => {
+    const { windows } = useWindowStore();
+    const data = windows.imagefile?.data;
+
+    if (!data) return null;
+
+    const { name, imageUrl } = data;
+
+    return (
+        <>
+            <div id="window-header">
+                <WindowControlls target="imgfile" />
+                <h2>{name}</h2>
+            </div>
+
+            <div className="p-5 bg-white">
+                {imageUrl ? (
+                    <div className="w-full">
+                        <img src={imageUrl} alt={name} className="w-full h-auto max-h-[70vh] object-contain rounded" />
+                    </div>
+                ) : null}
+            </div>
+        </>
+    )
+}
+
+const ImageWindow = WindowWrapper(ImageWindowContent, "imagefile");
+
+export default ImageWindow;
---
+++ src/windows/Resume.jsx
@@ -0,0 +1,33 @@
+import WindowWrapper from "#hoc/WindowWrapper";
+import WindowControlls from "#components/WindowControlls";
+import { Download } from "lucide-react";
+import { pdfjs } from "react-pdf";
+import { Document, Page } from "react-pdf";
+
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+
+const Resume = () => {
+    return (
+        <>
+            <div id="window-header">
+                <WindowControlls target="resume" />
+                <h2>Resume.pdf</h2>
+
+                <a href="/files/resume.pdf" download className="cursor-pointer" title="Download resume">
+                    <Download className="icon" />
+                </a>
+            </div>
+            <Document file="/files/resume.pdf" >
+                <Page pageNumber={1}
+                    renderTextLayer
+                    renderAnnotationLayer />
+            </Document>
+        </>
+    )
+}
+
+const ResumeWindow = WindowWrapper(Resume, 'resume');
+
+export default ResumeWindow;
